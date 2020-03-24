2151d29f494eeaedb7f797d61f618290dfa3b257
15874951564@163.com <zhaobaolong1>
2020-03-24 22:14:58 +0800

    学习中...

--- src/main/java/com/zbl/nio/NIOStudy01.java
+++ src/main/java/com/zbl/nio/NIOStudy01.java
@@ -6,13 +6,16 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
+import java.nio.channels.*;
+import java.util.Set;
 
 public class NIOStudy01 {
 	public static void main(String[] args) {
 
 	}
+
 	@Test
 	public void study01() throws IOException {
 		FileOutputStream fileOutputStream = new FileOutputStream("e:\\hello.txt");
@@ -24,17 +27,19 @@ public class NIOStudy01 {
 		fileOutputStream.close();
 
 	}
+
 	@Test
 	public void study02() throws Exception {
 		File file = new File("e:\\hello.txt");
 		FileInputStream fileInputStream = new FileInputStream(file);
 		FileChannel channel = fileInputStream.getChannel();
 
-		ByteBuffer allocate = ByteBuffer.allocate((int)file.length());
+		ByteBuffer allocate = ByteBuffer.allocate((int) file.length());
 		channel.read(allocate);
 		System.out.println(new String(allocate.array()));
 
 	}
+
 	@Test
 	public void study03() throws Exception {
 		File file = new File("e:\\hello.txt");
@@ -42,11 +47,11 @@ public class NIOStudy01 {
 		FileChannel channel = fileInputStream.getChannel();
 		FileOutputStream fileOutputStream = new FileOutputStream("hello2.txt");
 		FileChannel channel1 = fileOutputStream.getChannel();
-		ByteBuffer allocate = ByteBuffer.allocate((int)file.length());
-		while(true){
+		ByteBuffer allocate = ByteBuffer.allocate((int) file.length());
+		while (true) {
 			allocate.clear();
 			int read = channel.read(allocate);
-			if(read==-1){
+			if (read == -1) {
 				break;
 			}
 			allocate.flip();
@@ -55,6 +60,7 @@ public class NIOStudy01 {
 		fileInputStream.close();
 		fileOutputStream.close();
 	}
+
 	@Test
 	public void study04() throws Exception {
 		File file = new File("e:\\hello.txt");
@@ -65,5 +71,39 @@ public class NIOStudy01 {
 		long l = channel.transferTo(0, file.length(), channel1);
 		fileInputStream.close();
 		fileOutputStream.close();
+	}
+
+	@Test
+	public void study05() throws Exception {
+		//开启ServerSocketChannel
+		ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+		serverSocketChannel.socket().bind(new InetSocketAddress(6666));
+		//设置非阻塞
+
+		serverSocketChannel.configureBlocking(false);
+		//开启selector
+		Selector selector = Selector.open();
+		//将selector注册到server上
+		SelectionKey selectionKey = serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+		while (true) {
+			if (selector.select(1000) == 0) {
+				continue;
+			}
+			Set<SelectionKey> selectionKeys = selector.selectedKeys();
+			for (SelectionKey key : selectionKeys) {
+				if(key.isAcceptable()){
+					//如果是accept,生成一个channel
+					SocketChannel socketChannel = serverSocketChannel.accept();
+					//注册read事件到selector上
+					 socketChannel.register(selector, SelectionKey.OP_READ,ByteBuffer.allocate(1024));
+				}
+				if(key.isReadable()){
+					SocketChannel channel = (SocketChannel) key.channel();
+					ByteBuffer attachment = (ByteBuffer) key.attachment();
+					channel.read(attachment);
+					System.out.println(new String(attachment.array()));
+				}
+			}
+		}
 	}
 }
