987ebdd839b7efe6d25b3b1a3cd6a4ea89a4b31a
root <357760876@qq.com>
2020-04-22 10:45:00 +0800

    对于实战的学习demo

--- src/main/java/com/zbl/nio/neyInAcion/c01/EchoServerHandler.java
+++ src/main/java/com/zbl/nio/neyInAcion/c01/EchoServerHandler.java
@@ -4,8 +4,9 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.CharsetUtil;
+import lombok.extern.slf4j.Slf4j;
 
-//@Slf4j
+@Slf4j
 public class EchoServerHandler extends ChannelInboundHandlerAdapter {
 
     @Override
