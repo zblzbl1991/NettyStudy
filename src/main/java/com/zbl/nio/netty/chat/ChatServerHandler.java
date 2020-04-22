89f637ebf9ceb8d18ec9511c426cfa8324634393
zblzbl1991 <357760876@qq.com>
2020-04-22 23:11:17 +0800

    学习中... 编写ChatServer 2020-04-22

--- src/main/java/com/zbl/nio/netty/chat/ChatServerHandler.java
+++ src/main/java/com/zbl/nio/netty/chat/ChatServerHandler.java
@@ -1,19 +1,63 @@
 package com.zbl.nio.netty.chat;
 
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.group.ChannelGroup;
 import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.util.concurrent.GlobalEventExecutor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
-public class ChatServerHandler extends SimpleChannelInboundHandler {
+@Slf4j
+public class ChatServerHandler extends SimpleChannelInboundHandler<String> {
 	//定义一个channel组
 	//GlobalEventExecutor 全局事件执行器
 	ChannelGroup channels = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+	SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	@Override
-	protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+
+		Channel channel = ctx.channel();
+		channels.forEach(ch->{
+			if(channel!=ch){
+				ch.writeAndFlush(sdf.format(new Date())+"[客户]"+channel.remoteAddress()+"说:"+msg+"\n");
+			}else{
+				ch.writeAndFlush(sdf.format(new Date())+"[你]说:"+msg+"\n");
+			}
+		});
+
+	}
+
+	@Override
+	public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+		Channel channel = ctx.channel();
+		channels.writeAndFlush("[客户端]"+ channel.remoteAddress()+"加入了聊天");
+		channels.add(channel);
+	}
+
+	@Override
+	public void channelActive(ChannelHandlerContext ctx) throws Exception {
+		log.info("客户端{}进行了连接",ctx.channel().id().asLongText());
+
+	}
+
+	@Override
+	public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+		log.info("客户端{}断开了连接",ctx.channel().id().asLongText());
+	}
+	@Override
+	public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+		Channel channel = ctx.channel();
+		channels.writeAndFlush("[客户端]"+channel.remoteAddress()+"离开了聊天");
+	}
+
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		cause.printStackTrace();
+		ctx.close();
 	}
 }
